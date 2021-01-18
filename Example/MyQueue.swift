2ebce2627fed1928fd11e7db9805210c9cd8bf33
HoangNM <hoangnm@ttc-solutions.com>
2021-01-18 18:31:35 +0700

    - Hola

--- Example/MyQueue.swift
+++ Example/MyQueue.swift
@@ -10,27 +10,55 @@ import Foundation
 class Queue<T> {
     private var elements: [T] = []
     
+    let queue = DispatchQueue(label: "customQueue")
     func pushDispatch(_ element: T) {
-        self.elements.append(element)
+        queue.async {
+            self.elements.append(element)
+        }
     }
     
     func popDispatch() -> T? {
-        return self.elements.removeFirst()
+        var element: T?
+        queue.sync {
+            guard !elements.isEmpty else {
+                return
+            }
+            element = self.elements.removeFirst()
+        }
+        return element
     }
     
+    let semaphore = DispatchSemaphore(value: 1)
     func pushSemaphore(_ element: T) {
+        semaphore.wait()
         self.elements.append(element)
+        semaphore.signal()
     }
     
     func popSemaphore() -> T? {
-        return self.elements.removeFirst()
+        var element: T?
+        semaphore.wait()
+        if !elements.isEmpty {
+            element = self.elements.removeFirst()
+        }
+        semaphore.signal()
+        return element
     }
     
+    let lock = NSLock()
     func pushLock(_ element: T) {
+        lock.lock()
         self.elements.append(element)
+        lock.unlock()
     }
     
     func popLock() -> T? {
-        return self.elements.removeFirst()
+        var element: T?
+        lock.lock()
+        if !elements.isEmpty {
+            element = self.elements.removeFirst()
+        }
+        lock.unlock()
+        return element
     }
 }
